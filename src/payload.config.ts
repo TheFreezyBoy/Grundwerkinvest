52245705e74c83a3f320a7cd93613fbe6981371a
Denys <hitsugaru4@gmail.com>
2026-04-27 02:16:00 +0200

    commit

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -36,7 +36,7 @@ export default buildConfig({
   },
   db: postgresAdapter({
     pool: {
-      connectionString: process.env.DATABASE_URL || '',
+      connectionString: process.env.DATABASE_URI || '',
     },
   }),
   sharp,
